777b765f5921ad75bfb198d67a06fda772ce25f7
Nick <mcmasternick5572@gmail.com>
2024-01-16 15:20:27 -0500

    Prototype

---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    	public GameObject currentScreen;
+    	public GameObject[] screens;
+
+	public GameObject initPanel;
+
+    	// Start is called before the first frame update
+    	void Awake()
+    	{
+        	//SetActivePanel(initPanel);
+    	}
+
+    	// Update is called once per frame
+    	void Update()
+    	{
+
+    	}
+
+
+	public void SetActivePanel(ThisPanel panel)
+	{
+		currentScreen.SetActive(false);
+		currentScreen = screens[(int)panel.myPanel];
+		currentScreen.SetActive(true);
+
+		Debug.Log("panel has been swapped");
+	}
+}
+
+
+public enum Panels
+{
+	OVERVIEW, LEFT_CIRCLE, MIDDLE_CIRCLE, RIGHT_CIRCLE
+
+}
